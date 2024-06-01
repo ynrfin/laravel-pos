c8b6e55663a74b161716ebdfbbfc4abff8466578
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2024-06-01 21:32:22 +0700

    fix: delete unnecessary codes

--- routes/web.php
+++ routes/web.php
@@ -9,18 +9,13 @@ Route::view('/', 'welcome');
 Route::get('/barcode', function(){
     // return "wel";
     $barcode = DNS1D::getBarcodeSVG('COBA-1243', 'C128A', 1, 50);
-    $b = new BarcodeGenerator();
-
-    // $barcode = $b->getBarcodeSVG('ABC123', 'C128A',2,50,array(1,1,1), true);
 
     $product = new Product();
     $product->name = "Tas Tayo";
     $product->sell_price = 34000;
     $pdf = Pdf::loadView("products.barcode", ["barcode" => $barcode, "product" => $product ]);
     return $pdf->stream();
-    // dd($barcode);
     // return view("products.barcode", ["barcode" => $barcode, "product" => $product ]);
-    // return DNS1D::getBarcodeSVG('4445645656', 'C128A');
 });
 
 Route::view('dashboard', 'dashboard')
